a7ca22da76cc954479622cd46791ca21eaea6f77
galgazur <galgazur@gmail.com>
2014-03-13 19:58:57 +0100

    script

--- main.m
+++ main.m
@@ -2,11 +2,8 @@ disp('Generera slumpmassiga tal');
 
 addpath Functions;
 
-
-disp(linearCongruency(100,97,97,97,200));
-
 X = 99*99*99;
-AllNumbers = ones(X,20);
+AllNumbers = ones(X,100);
 
 modulus = 100;
 for a=1:99
@@ -16,7 +13,7 @@ for a=1:99
             disp(['Optimal for modulus:',num2str(modulus),' params multiplier:',num2str(a),' increment:',num2str(b)])
         end
         for c=1:99
-            AllNumbers((a-1)*99*99 + (b-1)*99 + c,:) = linearCongruency(100,a,b,c,20)+1;
+            AllNumbers((a-1)*99*99 + (b-1)*99 + c,:) = linearCongruency(100,a,b,c,100)+1;
         end
     end
 end
@@ -26,7 +23,6 @@ for y=1:X
     for z=1:20
         p = AllNumbers(y,z);
         Occurrences(p) = Occurrences(p)+1;
-
     end
 end
     

b24ece97fa00445028d184914daae834f7078ff2
ranby <erik.ranby@gmail.com>
2014-04-03 12:16:49 +0200

    coupon test works

--- Functions/stirling.m
+++ Functions/stirling.m
@@ -9,7 +9,7 @@ else
    div = 1/factorial(k);
    sum = 0;
    for i=0:k-1
-      a = (-1).^(k-i)*nchoosek(k, i)*i.^n;
+      a = ((-1).^i)*nchoosek(k, i)*(k-i).^n;
       sum=sum+a;
    end
    output = div*sum;

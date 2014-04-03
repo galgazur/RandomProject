dd220f3e151eaa3e2f7492b40604df2a713a2f42
galgazur <galgazur@gmail.com>
2014-04-03 11:12:55 +0200

    gap test!

--- Functions/gaptest.m
+++ Functions/gaptest.m
@@ -1,20 +1,24 @@
 function [ COUNT ] = gaptest( indata, a, b, minNumber, maxNumber, t, n)
 
 len = length(indata);         %this is the amount of numbers
-COUNT = zeros(t);           %no ranges saved
-r = 0;                      %no range initially
+COUNT = zeros(1,t);           %no ranges saved
+r = 1;                      %no range initially
+s = 0;                      %# of ranges saved
+
+disp(['t: ',num2str(t)]);
+
 
 if(a>=b)
     error('Alpha is bigger or equal to beta!');
 end
-for j = -1:len
+for j = 1:len
     if( (indata(j)>=a)&&(indata(j)<b) )
         COUNT(r) = COUNT(r)+1;
-        r = 0;
+        r = 1;
         s = s + 1;
     else
        r = r+1;
-       if(r>t)
+       if(r>=t)
            error('A range bigger than t was found!');
        end
     end
--- Functions/gaptestWrapper.m
+++ Functions/gaptestWrapper.m
@@ -1,11 +1,22 @@
-function [ result ] = gaptestWrapper( sequence, a, b, minNumber, maxNumber, n, table_value)
+function [ result ] = gaptestWrapper( sequence, a, b, minNumber, maxNumber, n, t, table_value)
 %GAPTESTWRAPPER Summary of this function goes here
 %   Detailed explanation goes here
 
-expected = zeros(length(sequence)/);
-observed = gaptest( sequence, a, b, minNumber, maxNumber, length(expected), n);
+expected = zeros(1,t);
+interval = maxNumber-minNumber;
+p = (b-a)/interval;
+
+for i=0:(t-1)
+    if(i>0)
+        num = n*p*((1-p)^i);
+    else
+        num = n*p;
+    end
+    expected(i+1) = num;
+end
+
+observed = gaptest( sequence, a, b, minNumber, maxNumber, t, n);
 
 result = chi_two_test(observed, expected, table_value)
 
 end
-

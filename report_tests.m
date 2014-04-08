4dce6f80da1add1959b952ed9935bfcd1383ac67
galgazur <galgazur@gmail.com>
2014-04-08 13:28:57 +0200

    Linear congruency med 2^31 - 1 och 48271, 0 istället

--- report_tests.m
+++ report_tests.m
@@ -1,5 +1,26 @@
 addpath Functions;
 
+
+seed = 999;
+
+
+
+mtg1K = mersenne_twister(seed,1000);
+mtg10K = mersenne_twister(seed,10000);
+mtg100K = mersenne_twister(seed,100000);
+mtg1000K = mersenne_twister(seed,1000000);
+
+lcg1K = lcg(seed, 1000);
+lcg10K = lcg(seed, 10000);
+lcg100K = lcg(seed, 100000);
+lcg1000K = lcg(seed, 1000000);
+
+for i=1:8
+
+    
+    
+end
+
 lcg3000raw = lcg(14, 3000);
 lcg3000f = lcg3000raw*10;
 lcg3000 = floor(lcg3000raw*10+1);
@@ -12,7 +33,7 @@ mtg3000 = floor(mtg3000raw*10+1);
 
 
 %GAP TEST
-gaptest = gaptestWrapper(lcg3000, 4, 5, min(lcg3000), max(lcg3000), 100, 500, 340);
+gaptest = gaptestWrapper(lcg3000, 0.5, 1.5, min(lcg3000), max(lcg3000), 100, 500, 340);
 if(gaptest==1)
     disp('Gap test determined that this sequence showed non-random patterns.');
 else
